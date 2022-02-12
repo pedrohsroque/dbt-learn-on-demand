117d67636da6ede1dbf89bffb17e7e8029bccf81
Pedro Roque <pedrohsroque@gmail.com>
2022-02-12 02:00:37 -0300

    modify file

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,7 +1,6 @@
 -- How to reference other models
 -- select * from {{ ref('stg_customers')}}
 
--- order_id, customer_id, amount
 with orders as (
     select order_id, customer_id from {{ ref('stg_orders')}}
 ),
